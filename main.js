104d876a1c9f573336dbbb92fe3e18245cb4d5e2
Marcos Rigo <marcos.rigo.10@gmail.com>
2022-09-24 13:50:48 -0300

    Subiendo todo el Challenge 14

---
+++ main.js
@@ -0,0 +1,27 @@
+let edad = Number(prompt("Ingrese su edad y le diré que canción le gusta"));
+
+if (edad >= 1 && edad <= 8) {
+    console.log(" Escuchá este tema en YouTube: Peppa Pig en Español 🎵 Ella es Peppa Pig | Canta con Peppa | Pepa la cerdita");
+}else {
+    if(edad > 8 && edad <=20){
+        console.log("Probá con el siguiente tema en YouTube: Paulo Londra - Party en el Barrio (feat. Duki) [Official Video]");
+    }else{
+        if(edad > 20 && edad <= 40){
+            console.log("A vos te puede interesar este enganchadito en YouTube: Los mejores 10 temas de cumbia de la historia");
+        }
+        else{
+            if (edad > 40){
+                console.log("Copie y pegue este título en el buscador de YouTube: Despeinada / Corazón Contento / Viva la Vida / Media Novia / Camelia / Bienvenido Amor");
+            }
+            else {
+                alert("Ingrese su edad correctamente y en número enteros por favor...");
+            }    
+        }
+    }
+
+}
+
+
+
+
+
